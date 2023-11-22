f42916d98753e3fa1a349ec3238c8aeb21fd88c9
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-11-22 13:14:57 +0100

    Initial commit

---
+++ src/main/java/com/example/data22aexamprojectlnl2/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.example.data22aexamprojectlnl2.controller;
+
+import com.example.data22aexamprojectlnl2.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/send-email")
+    public void sendEmail() {
+        String to = "mgrosenp@outlook.dk";
+        String subject = "Hello from SMTP";
+        String body = "Nu kan vi sende mails med vores service";
+
+        emailService.sendEmail(to, subject, body);
+    }
+}
