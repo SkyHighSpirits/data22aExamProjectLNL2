59748bbd33fdad10eba4446221e7b6d40c40f701
Nicolai Ene <35382666+SkyHighSpirits@users.noreply.github.com>
2023-11-23 16:48:32 +0100

    A ton of bugfixing and implementation attempts of springsecurity filthers. With partial success
    Need to make propper SecurityConfig later

---
+++ src/main/java/com/example/data22aexamprojectlnl2/Config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.data22aexamprojectlnl2.Config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.web.cors.CorsConfiguration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // Create a custom CORS configuration that allows any origin, method, and header
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+
+        // Configure CORS with the custom configuration
+        Customizer<CorsConfigurer<HttpSecurity>> corsCustomizer = (cors) ->
+                cors.configurationSource(request -> corsConfig);
+
+        http
+                .csrf().disable()
+                .cors(corsCustomizer)
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
+    
+
