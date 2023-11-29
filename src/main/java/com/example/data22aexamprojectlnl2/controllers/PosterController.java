93849d7dc0e5c041a5618efdeca5a8c701be41c0
LutherMGP <mgrosenp@outlook.dk>
2023-11-29 16:19:31 +0100

    Indførsel af pagination.

--- src/main/java/com/example/data22aexamprojectlnl2/controllers/PosterController.java
+++ src/main/java/com/example/data22aexamprojectlnl2/controllers/PosterController.java
@@ -1,21 +1,17 @@
 package com.example.data22aexamprojectlnl2.controllers;
 
 import com.example.data22aexamprojectlnl2.models.Image;
-import com.example.data22aexamprojectlnl2.repositories.ImageRepository;
-import com.example.data22aexamprojectlnl2.services.ImageService;
 import com.example.data22aexamprojectlnl2.models.Poster;
+import com.example.data22aexamprojectlnl2.repositories.ImageRepository;
 import com.example.data22aexamprojectlnl2.repositories.PosterRepository;
+import com.example.data22aexamprojectlnl2.services.ImageService;
 import com.example.data22aexamprojectlnl2.services.PosterService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,53 +21,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 @CrossOrigin
 @Controller
-public class PosterController
-{
+public class PosterController {
     @Autowired
+    private PosterRepository posterRepository;
+    @Autowired
+    private PosterService posterService;
+    @Autowired
+    private ImageService imageService;
+    @Autowired
+    private ImageRepository imageRepository;
 
-    PosterRepository posterRepository;
-    @Autowired
-    PosterService posterService;
-    @Autowired
-
-    ImageService imageService;
-    @Autowired
-    ImageRepository imageRepository;
-
-    // flyt til image controller
     @GetMapping("/getImages")
-    public ResponseEntity<List<Image>> getAllImagesByPosterId(@RequestParam("poster_id") int poster_id)
-    {
+    public ResponseEntity<List<Image>> getAllImagesByPosterId(@RequestParam("poster_id") int poster_id) {
         List<Image> images = imageService.getImagesByPosterId(poster_id);
         return new ResponseEntity<>(images, HttpStatus.OK);
     }
 
-
-
-
-    // dette er faktisk en /postPost endpoint. der skal tilføjes Description og Titel.
     @PostMapping("/createPost")
-    public ResponseEntity<String> createPoster( @RequestParam("title") String title,
-                                                @RequestParam("description") String description,
-                                                @RequestParam("images") MultipartFile[] images) {
-
+    public ResponseEntity<String> createPoster(@RequestParam("title") String title,
+                                               @RequestParam("description") String description,
+                                               @RequestParam("images") MultipartFile[] images) {
         try {
-            // Create a new Poster
             Poster poster = new Poster();
             poster.setPoster_Title(title);
             poster.setPoster_Description(description);
 
-            // Save the Poster entity
             Poster savedPoster = posterService.savePoster(poster);
 
-            // Save the associated images
             for (MultipartFile imageFile : images) {
                 Image image = new Image();
                 image.setByte_img(imageFile.getBytes());
-                image.setPoster(savedPoster); // Set the Poster entity for the image
+                image.setPoster(savedPoster);
                 imageService.saveImage(image);
             }
 
@@ -83,15 +65,17 @@ public class PosterController
     }
 
     @GetMapping("/getPosts")
-    public ResponseEntity<List<Map<String, Object>>> getAllPosts() {
+    public ResponseEntity<List<Map<String, Object>>> getAllPosts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size) {
         try {
+            Pageable pageable = PageRequest.of(page, size);
             List<Map<String, Object>> result = new ArrayList<>();
 
-            for (Poster poster : posterRepository.findAll()) {
+            for (Poster poster : posterRepository.findAll(pageable)) {
                 Map<String, Object> posterInfo = new HashMap<>();
                 posterInfo.put("poster", poster);
 
-                // Henter billeder ved hjælp af imageService
                 List<Image> posterImages = imageService.getImagesByPosterId(poster.getId());
                 posterInfo.put("images", posterImages);
 
@@ -103,4 +87,6 @@ public class PosterController
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+    // Eventuelle andre metoder...
 }
